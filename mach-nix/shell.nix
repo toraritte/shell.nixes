c9ecd4dfcfa962e4bd68839909be10981d022287
Attila Gulyas <toraritte@gmail.com>
2021-04-18 14:42:56 +0000

    Re-indent mach-nix shell.nix

--- mach-nix/shell.nix
+++ mach-nix/shell.nix
@@ -8,19 +8,25 @@ let
   # e.g., "/nix/store/gk9x7syd0ic6hjrf0fs6y4bsd16zgscg-source"
   # (Try any of the `fetchTarball` commands  below  in `nix repl`, and it
   #  will print out the path.)
-  fetchedPinnedTarball = builtins.fetchTarball {
-    name = "mach-nix";
-    url = "https://github.com/DavHau/mach-nix/tarball/${mach_nix_version}";
-    sha256 = "0w6i3wx9jyn29nnp6lsdk5kwlffpnsr4c80jk10s3drqyarckl2f";
-  };
+  fetchedPinnedTarball =
+    builtins.fetchTarball
+      { name = "mach-nix";
+        url = "https://github.com/DavHau/mach-nix/tarball/${mach_nix_version}";
+        sha256 = "0w6i3wx9jyn29nnp6lsdk5kwlffpnsr4c80jk10s3drqyarckl2f";
+      }
+  ;
   mach_nix = (import fetchedPinnedTarball {}).mach-nix;
 
   nixpkgs_commit = "cfed29bfcb28259376713005d176a6f82951014a";
-  pinned_pkgs = import (builtins.fetchTarball {
-    name = "nixpkgs";
-    url = "https://github.com/nixos/nixpkgs/tarball/${nixpkgs_commit}";
-    sha256 = "034m892hxygminkj326y7l3bp4xhx0v154jcmla7wdfqd23dk5xm";
-  }) { config = {}; overlays = []; };
+  pinned_pkgs =
+    import
+      ( builtins.fetchTarball
+          { name = "nixpkgs";
+            url = "https://github.com/nixos/nixpkgs/tarball/${nixpkgs_commit}";
+            sha256 = "034m892hxygminkj326y7l3bp4xhx0v154jcmla7wdfqd23dk5xm";
+          }
+      ) { config = {}; overlays = []; }
+  ;
 in
   # If  `nix-shell`  is  simply  called  with  this  Nix
   # expression,  then  the  used Nixpkgs  link  will  be
@@ -39,6 +45,5 @@ in
   # which will override everything above.
   { pkgs ? pinned_pkgs }:
 
-  pkgs.mkShell {
-    buildInputs = [ mach_nix ];
-  }
+  pkgs.mkShell
+    { buildInputs = [ mach_nix ]; }
