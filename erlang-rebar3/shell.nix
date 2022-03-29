c9b81e3291fc05da9fa740d0b237d1116c3a8d17
Attila Gulyas <toraritte@gmail.com>
2022-03-28 23:48:02 -0400

    Update erlang/rebar3 shell.nix
    Update major version and  updated fetching Nixpkgs from Github
    instead of pointing to a local clone to make it more portable.

--- erlang-rebar3/shell.nix
+++ erlang-rebar3/shell.nix
@@ -5,24 +5,52 @@
 # of `<nixpkgs>` and `~` in Nix expressions (towards the end of that
 # section).
 ####################################################################
-#               VVVVVVVVVVVVVVVV
-{ pkgs ? import ~/clones/nixpkgs {} }:
 
-pkgs.mkShell {
+# #               VVVVVVVVVVVVVVVV
+# { pkgs ? import ~/clones/nixpkgs {} }:
 
-  buildInputs = with pkgs; [
-    beam.packages.erlangR22.erlang
-    beam.packages.erlangR22.rebar3
-    git
-  ];
+let
+  nixpkgs_commit = "f5054121cb287317c4ca8c409ef9c68f36658013"; # 2022-03-29T03:03:59Z
+  pinnedNixpkgsGithubURL = "https://github.com/NixOS/nixpkgs/archive/${nixpkgs_commit}.tar.gz";
+  # Going  with  this  variant  of `fetchTarball`  as  it  doesn't
+  # require the sha256 dance
+  fetchedPinnedTarball = builtins.fetchTarball pinnedNixpkgsGithubURL;
 
-  ####################################################################
-  # Without  this, almost  everything  fails with  locale issues  when
-  # using `nix-shell --pure` (at least on NixOS).
-  # See
-  # + https://github.com/NixOS/nix/issues/318#issuecomment-52986702
-  # + http://lists.linuxfromscratch.org/pipermail/lfs-support/2004-June/023900.html
-  ####################################################################
+  # The downloaded archive will be (temporarily?) housed in the Nix store
+  # e.g., "/nix/store/gk9x7syd0ic6hjrf0fs6y4bsd16zgscg-source"
 
-  LOCALE_ARCHIVE = if pkgs.stdenv.isLinux then "${pkgs.glibcLocales}/lib/locale/locale-archive" else "";
-}
+  # nixpkgs_sha256 = "01bmiqndp1czwjw87kp21dvxs0zwv7yypqlyp713584iwncxjv0r";
+  # fetchedPinnedTarball =
+  #   builtins.fetchTarball
+  #     { name = "nixpkgs";
+  #       url = pinnedNixpkgsGithubURL;
+  #       sha256 = nixpkgs_sha256;
+  #     }
+  # ;
+
+in
+
+  { pkgs ? import fetchedPinnedTarball {} }:
+
+  pkgs.mkShell {
+
+    buildInputs = with pkgs; [
+      # Not sure  what the best  way to  find the latest  major Erlang
+      # version  available  in  Nixpkgs  so   I  usually  just  go  to
+      # nixpkgs/pkgs/development/interpreters/erlang/  and see  what's
+      # there.
+      beam.packages.erlangR24.erlang
+      beam.packages.erlangR24.rebar3
+      git
+    ];
+
+    ####################################################################
+    # Without  this, almost  everything  fails with  locale issues  when
+    # using `nix-shell --pure` (at least on NixOS).
+    # See
+    # + https://github.com/NixOS/nix/issues/318#issuecomment-52986702
+    # + http://lists.linuxfromscratch.org/pipermail/lfs-support/2004-June/023900.html
+    ####################################################################
+
+    LOCALE_ARCHIVE = if pkgs.stdenv.isLinux then "${pkgs.glibcLocales}/lib/locale/locale-archive" else "";
+  }
